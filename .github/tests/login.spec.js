c49f4bedf1279b94c17609236ce9843ef8f083e9
KatyaF <Ekaterina_Farafontova@epam.com>
2026-07-29 18:26:57 +0400

    Update login.spec.js

--- .github/tests/login.spec.js
+++ .github/tests/login.spec.js
@@ -1,1 +1,15 @@
+```javascript
+const { test, expect } = require('@playwright/test');
+
+test.describe('Login Flow Automation Tests', () => {
+  let page;
+
+  test.beforeEach(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.setViewportSize({ width: 1920, height: 1080 });
+  });
+
+  test.afterEach(async () => {
+    await page?.close();
+  });
 
